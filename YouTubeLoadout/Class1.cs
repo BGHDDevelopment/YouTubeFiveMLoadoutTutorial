d4aa6969b6473b172dc6f39148ac2df363b66d31
BGHDDevelopment <ceo@bghddevelopment.com>
2021-01-31 17:14:10 -0800

    Initial commit

---
+++ YouTubeLoadout/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace YouTubeLoadout
+{
+    public class Class1 : BaseScript
+    {
+
+        public Class1()
+        {
+            Tick += OnTick;
+            EventHandlers["playerSpawned"] += new Action<Vector3>(playerSpawned);
+        }
+
+        private async Task OnTick()
+        {
+            await Task.Delay(100);
+        }
+
+        private void playerSpawned([FromSource] Vector3 spawn)
+        {
+            Game.PlayerPed.Weapons.RemoveAll();
+            Game.PlayerPed.Weapons.Give(WeaponHash.StunGun, 100, false, true);
+            Game.PlayerPed.Weapons.Give(WeaponHash.Pistol, 100, false, true);
+            TriggerEvent("chat:addMessage", new
+            {
+            color = new[] {255,0,0},
+            multiline = true,
+            args = new[] {"[LOADOUT]", "^3You have received the loadout!"}
+            });
+        }
+    }
+}
---
+++ YouTubeLoadout/bin/Debug/YTLoadout/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games {"gta5"}
+
+author 'BGHDDevelopment'
+description 'Test Resource'
+version '1.0.0'
+
+client_script {
+'YouTubeLoadout.net.dll'
+}
